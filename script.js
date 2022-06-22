689793898c1249ce1dee04f86e284713bd30ab49
devbobnwaka <62200084+devbobnwaka@users.noreply.github.com>
2022-06-22 20:58:39 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('Name: Chibueze Bob Nwaka')
+alert('Height: 5.6ft')
+alert('Country: Nigeria')
+
+
+console.log('Name: Chibueze Bob Nwaka')
+console.log('Height: 5.6ft')
+console.log('Country: Nigeria')
